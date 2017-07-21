b0b72ed3a02a3da56f816d7e09869b66a288f0eb
drewtheghost <thatonedrew22@gmail.com>
2017-07-21 17:08:11 -0500

    Update logManager.js

--- handlers/logManager.js
+++ handlers/logManager.js
@@ -1,48 +1,36 @@
-"use strict";
-const fs = require("fs");
+"use strict"
+const fs = require("fs")
 class Logs {
-    
+
     constructor() {
-        this.date_now = new Date()
+        this.date_now = new Date().toString().split(' ').join("_")
     }
-    
+
     run() {
         if (!fs.existsSync(__dirname + "/../logs/")) {
             fs.mkdirSync(__dirname + "/../logs/")
         } else {
-            var date = new Date()
-            var datefile = "" + this.date_now + ""
-            datefile.split(" ").join("_")
-            if (!fs.existsSync(__dirname + "/../logs/" + datefile + ".noire_log")) {
-                fs.appendFileSync(__dirname + "/../logs/" + datefile + ".noire_log", '(' + new Date() + ') [INFO] Begun logging for today!')
+            if (!fs.existsSync(__dirname + "/../logs/" + this.date_now + ".noire_log")) {
+                fs.appendFileSync(__dirname + "/../logs/" + this.date_now + ".noire_log", '(' + new Date() + ') [INFO] Begun logging for today!')
             }
         }
     }
-    
+
     info(message) {
-            var date = new Date()
-            var datefile = "" + this.date_now + ""
-            datefile.split(" ").join("_")
-        if (fs.existsSync(__dirname + "/../logs/" + datefile + ".noire_log")) {
-            fs.appendFileSync(__dirname + "/../logs/" + datefile + ".noire_log", '\n(' + new Date() + ') [INFO] ' + message)   
+        if (fs.existsSync(__dirname + "/../logs/" + this.date_now + ".noire_log")) {
+            fs.appendFileSync(__dirname + "/../logs/" + this.date_now + ".noire_log", '\n(' + new Date() + ') [INFO] ' + message)
         }
     }
-    
+
     warn(message) {
-            var date = new Date()
-            var datefile = "" + this.date_now + ""
-            datefile.split(" ").join("_")
-        if (fs.existsSync(__dirname + "/../logs/" + datefile + ".noire_log")) {
-            fs.appendFileSync(__dirname + "/../logs/" + datefile + ".noire_log", '\n(' + new Date() + ') [WARN] ' + message)   
+        if (fs.existsSync(__dirname + "/../logs/" + this.date_now + ".noire_log")) {
+            fs.appendFileSync(__dirname + "/../logs/" + this.date_now + ".noire_log", '\n(' + new Date() + ') [WARN] ' + message)
         }
     }
-    
+
     error(message) {
-            var date = new Date()
-            var datefile = "" + this.date_now + ""
-            datefile.split(" ").join("_")
-        if (fs.existsSync(__dirname + "/../logs/" + datefile + ".noire_log")) {
-            fs.appendFileSync(__dirname + "/../logs/" + datefile + ".noire_log", '\n(' + new Date() + ') [ERROR] ' + message)   
+        if (fs.existsSync(__dirname + "/../logs/" + this.date_now + ".noire_log")) {
+            fs.appendFileSync(__dirname + "/../logs/" + this.date_now + ".noire_log", '\n(' + new Date() + ') [ERROR] ' + message)
         }
     }
 }
